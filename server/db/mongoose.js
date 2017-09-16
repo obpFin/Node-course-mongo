26a0e81e4618d85fe7d550872c2657a3a8284de6
Oskari Peltonen <oskari.peltonen@outlook.com>
2017-09-16 22:19:53 +0300

    setup app for Heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,10 +2,10 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 
-let connection = mongoose.connect('mongodb://localhost:27017/TodoApp', {
+let connection = mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp', {
   useMongoClient: true
 }).then(db => {
-  console.log('MongoDB is connected')
+  //console.log('MongoDB is connected')
 }).catch(error => {
   console.log(error)
 })
